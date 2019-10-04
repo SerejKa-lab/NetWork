5fd414c67219b8325b9fc343f40f2a1daa642ec2
SerejKa-lab <seruogacom@mail.ru>
2019-10-04 09:57:18 +0300

    aded lessons project

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return(
+        <div className='content'>
+                <div>
+                    <img src="https://s1.1zoom.me/big7/297/Canada_Mountains_Scenery_488936.jpg"></img>
+                </div>
+                <div>
+                    Avatar + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            Post1
+                        </div>
+                        <div>
+                            Post2
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default Profile;
