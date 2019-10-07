b1cab39e761ce32551a2d4356158ec7dadf25116
SerejKa-lab <seruogacom@mail.ru>
2019-10-07 14:10:23 +0300

    Lesson 21 Dialogs component frame is created

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import style from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.conversationNames}>
+                <div className={style.name}>
+                    ConversationName1
+               </div>
+                <div className={style.name}>
+                    ConversationName2
+               </div>
+                <div className={style.name}>
+                    ConversationName3
+               </div>
+                <div className={style.name}>
+                    ConversationName4
+               </div>
+            </div>
+            <div className={style.conversations}>
+                <div>
+                    Conversation1
+                </div>
+                <div>
+                    Conversation2
+                </div>
+                <div>
+                    Conversation3
+                </div>
+                <div>
+                    Conversation4
+                </div>
+            </div>
         </div>
     )
 }
