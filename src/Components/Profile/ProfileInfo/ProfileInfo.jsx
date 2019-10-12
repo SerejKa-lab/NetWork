2f2970d13221d4530d6eb81b4c76f77f12edd4cb
SerejKa-lab <seruogacom@mail.ru>
2019-10-12 12:30:00 +0300

    Profile component created

---
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './ProfileInfo.module.css';
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div className={style.image}>
+                <img src="https://s1.1zoom.me/big7/297/Canada_Mountains_Scenery_488936.jpg"></img>
+            </div>
+            <div className={style.description}>
+                Avatar + description
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo;
