779f152c423d7b7f21b552c40cb0923a08fd83b0
SerejKa-lab <seruogacom@mail.ru>
2019-11-13 13:44:26 +0300

    reducers are refactored: 'switch case' operator used instead 'if'

--- src/Redux/dialogsReducer.js
+++ src/Redux/dialogsReducer.js
@@ -3,28 +3,31 @@ const ADD_MESSAGE = 'ADD-MESSAGE';
 
 const dialogsReducer = (state, action) => {
     
-    if ( action.type === SET_NEW_MESSAGE_TEXT ){
-        state.dialogNames.forEach( (name) => {
-            if ( action.id === name.id ) {
-               name.newMessage = action.newMessageText;
-            }
-        } ) 
+    switch ( action.type) {
+        case SET_NEW_MESSAGE_TEXT: 
+            state.dialogNames.forEach( (name) => {
+                if ( action.id === name.id ) {
+                name.newMessage = action.newMessageText;
+                }
+            } );
+            return state;
+   
+        case ADD_MESSAGE:
+            state.dialogNames.forEach( (name) => {
+                if ( action.id === name.id && name.newMessage !== '' ) {
+                    let newMessage = {
+                        text: name.newMessage,
+                        id: 'm2'
+                    };
+                name.dialogs.push(newMessage);
+                name.newMessage = '';
+                }
+            } ); 
+            return state;
         
+        default: return state;
+    
     }
-    if ( action.type === ADD_MESSAGE ){
-        state.dialogNames.forEach( (name) => {
-            if ( action.id === name.id && name.newMessage !== '' ) {
-                let newMessage = {
-                    text: name.newMessage,
-                    id: 'm2'
-                };
-               name.dialogs.push(newMessage);
-               name.newMessage = '';
-            }
-        } ) 
-    }
-
-    return state;
 }
 
 export default dialogsReducer;
--- src/Redux/profileReducer.js
+++ src/Redux/profileReducer.js
@@ -2,22 +2,26 @@ const ADD_POST = 'ADD-POST';
 const SET_POST_TEXT = 'SET-POST-TEXT';
 
 const profileReducer = (state, action) => {
-    if ( action.type === ADD_POST ) {
-        let newPost = {
-            name: 'Kolya',
-            message: state.postText,
-            like: '0',
-            id: 'Kolya',
-            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
-        };
-        state.postsData.push(newPost);
-        state.postText = '';
-    }
-    if ( action.type === SET_POST_TEXT ) {
-        state.postText = action.newPostText;
-    }
+    switch (action.type) { 
+        case ADD_POST:  
+            let newPost = {
+                name: 'Kolya',
+                message: state.postText,
+                like: '0',
+                id: 'Kolya',
+                profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+            };
+            state.postText = '';
+            state.postsData.push(newPost);
+            return state;
+    
+        case SET_POST_TEXT:   
+            state.postText = action.newPostText;
+            return state;
+       
 
-    return state;
+        default: return state;
+}
 }
 
 export default profileReducer;
