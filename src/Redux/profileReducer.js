dd752032a54e56ab4ec2ba4a5b65d53aa0403e6a
SerejKa-lab <seruogacom@mail.ru>
2019-11-15 00:07:27 +0300

    likeImage added to profilePage.postsData items

--- src/Redux/profileReducer.js
+++ src/Redux/profileReducer.js
@@ -5,23 +5,28 @@ let initialState = {
     postsData: [
         {
             name: 'Pavel', message: 'Hi. I am fine. Thank you!', like: '15', id: 'Pavel',
-            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+            likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
         },
         {
             name: 'Yuriy', message: 'Hello! How are you', like: '10', id: 'Yuriy',
-            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+            likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
         },
         {
             name: 'Katya', message: 'Would you like a cup of tee?', like: '17', id: 'Katya',
-            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+            likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
         },
         {
             name: 'Masha', message: "No, i'd like a glass of juice.", like: '17', id: 'Masha',
-            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+            likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
         },
         {
             name: 'EasyMan', message: 'Ok. Here it is!', like: '17', id: 'EasyMan',
-            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+            profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+            likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
         }
     ],
     postText: ''
@@ -35,7 +40,8 @@ const profileReducer = (state = initialState, action) => {
                 message: state.postText,
                 like: '0',
                 id: 'Kolya',
-                profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+                profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+                likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
             };
             state.postText = '';
             state.postsData.push(newPost);
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -9,23 +9,28 @@ let store = {
             postsData: [
                 {
                     name: 'Pavel', message: 'Hi. I am fine. Thank you!', like: '15', id: 'Pavel',
-                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+                    likesImage: 'https://d.wattpad.com/story_parts/60/images/15783fb988a9333849916129548.jpg'
                 },
                 {
                     name: 'Yuriy', message: 'Hello! How are you', like: '10', id: 'Yuriy',
-                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+                    likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
                 },
                 {
                     name: 'Katya', message: 'Would you like a cup of tee?', like: '17', id: 'Katya',
-                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+                    likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
                 },
                 {
                     name: 'Masha', message: "No, i'd like a glass of juice.", like: '17', id: 'Masha',
-                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+                    likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
                 },
                 {
                     name: 'EasyMan', message: 'Ok. Here it is!', like: '17', id: 'EasyMan',
-                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg'
+                    profileImage: 'https://img3.goodfon.ru/original/1400x1050/7/b7/cvetok-lepestki-priroda-fon-1988.jpg',
+                    likesImage: 'https://i.pinimg.com/474x/db/3a/bb/db3abbe34ca1a4568b408971cf4621ba.jpg'
                 }
             ],
             postText: ''
