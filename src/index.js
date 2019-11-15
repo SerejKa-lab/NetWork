ac6eb5c2fae81bcd1d7d31c8a96e2036d9aa0e96
SerejKa-lab <seruogacom@mail.ru>
2019-11-15 22:31:00 +0300

    App component is wraped in Provider component out of the react-redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './Redux/reduxStore';
+import { Provider } from 'react-redux';
 
 
 
 let renderEntireTree = () => {
-    ReactDOM.render( <App store = { store } />, document.getElementById('root') );
+    ReactDOM.render( 
+        <Provider store = { store }>
+            <App store = { store } />
+        </Provider>, document.getElementById('root') );
 };
 
 renderEntireTree( store.getState() );
