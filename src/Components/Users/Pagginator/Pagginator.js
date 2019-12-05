b11e478b84c919a1e992786d8b1925861e9dfeb4
SerejKa-lab <seruogacom@mail.ru>
2019-12-05 17:08:20 +0300

    Pagginator component

---
+++ src/Components/Users/Pagginator/Pagginator.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import styles from './Pagginator.module.css'
+
+
+
+class Pagginator extends React.Component {
+    
+    state = {
+        inputError: false,
+        displayNumber: null,
+        inputMode: false
+    }
+
+    componentDidMount() { this.setState({ displayNumber: this.props.currentPage }) };
+
+    setInputMode = (e) => {
+        this.setState({ inputMode: true })
+        this.checkDisplayValue(e)
+    }
+
+    disableInputMode = () => {
+        this.setState({ inputMode: false })
+    }
+
+    checkDisplayValue = (e) => {
+        const { startPage, finishPage } = this.props;
+        const nextPage = e.currentTarget.value;
+        if (nextPage >= startPage && nextPage <= finishPage) {
+            this.setState({ inputError: false, displayNumber: nextPage }) //получать в дисплей значение извне
+        } else {
+            this.setState({ displayNumber: nextPage }) //получать значение извне
+            this.setState({ inputError: true })
+        }
+    }
+
+    setPageOnEvent = (e) => {
+        const { startPage, finishPage, currentPage, dispatchNewPage } = this.props;
+        const nextPage = +e.currentTarget.value;
+        if (nextPage >= startPage && nextPage <= finishPage && nextPage !== currentPage) {
+            this.setState({ inputError: false });
+            dispatchNewPage(nextPage)
+        } else {
+            if (nextPage !== currentPage) this.setState({ inputError: true })
+        }
+    }
+
+    setPageOnKey = (e) => {
+        if (e.key === 'Enter') {
+            this.setPageOnEvent(e);
+            this.setState({ inputMode: false })
+        }
+    }
+
+
+    render() {
+
+        const { currentPage, startPage, finishPage, dispatchNewPage } = this.props
+
+        return (
+            <section className={styles.pagginatorWrapper}>
+                <nav className={styles.paginator}>
+                    {currentPage !== startPage
+                        && <span onClick={() => dispatchNewPage(startPage)}>First</span>}
+
+                    {this.state.inputMode
+                        ? <input type='number' value={this.state.displayNumber}
+                            min={startPage} max={finishPage}
+                            className={this.state.inputError ? styles.inpError : ''}
+                            onClick={this.setPageOnEvent}
+                            onKeyPress={this.setPageOnKey}
+                            onChange={this.checkDisplayValue}
+                            onBlur={this.disableInputMode} />
+                        : <input type='number' value={currentPage}
+                            min={startPage} max={finishPage}
+                            onInput={this.setInputMode}
+                            onClick={this.setPageOnEvent} />
+                    }
+                    {currentPage !== finishPage
+                        && <span onClick={() => dispatchNewPage(finishPage)}>Last</span>}
+                </nav>
+            </section>
+        )
+    }
+}
+
+export default Pagginator;
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -2,23 +2,17 @@ import React from 'react';
 import styles from './Users.module.css';
 import User from './User/User';
 import preloader from '../../Assets/Images/preloader.svg';
+import Pagginator from './Pagginator/Pagginator';
 
 
 class Users extends React.Component {
 
     state = {
-        inputError: false,
-        displayNumber: null,
         pagginator: false,
-        disabled: false
     }
-
-    componentDidMount() { this.setState({ displayNumber: this.props.currentPage }) };
-
 
     setEventPage = (page, inList) => {
         if (page !== this.props.currentPage) {
-            this.props.setCurrentPage(page);
             this.props.setUsers(page, inList);
         }
     }
@@ -27,45 +21,6 @@ class Users extends React.Component {
         const {currentPage, pagesCount} = this.props;
         if (currentPage < pagesCount) {
             this.setEventPage(currentPage+1, true);
-            currentPage+1 === pagesCount
-                ? this.setState({ displayNumber: currentPage+1, disabled: true })
-                : this.setState({ displayNumber: currentPage+1 })
-        }
-    }
-
-    setPageOnEvent = (e) => {
-        const { pagesCount } = this.props;
-        const nextPage = +e.currentTarget.value;
-        const { disabled } = this.state;
-        if (nextPage >= 1 && nextPage <= pagesCount) {
-            this.setState({ inputError: false });
-            nextPage === pagesCount
-                ? this.setState({ disabled: true })
-                : disabled && this.setState({ disabled: false })
-            this.setEventPage(nextPage)
-        } else this.setState({ inputError: true })
-        
-    }
-
-    setPageOnKey = (e) => {
-        if (e.key === 'Enter') this.setPageOnEvent(e);
-    }
-
-    setFirstLastPage = (page) => {
-        page === this.props.pagesCount
-            ? this.setState({ displayNumber: page, disabled: true })
-            : this.setState({ displayNumber: page, disabled: false })
-        this.setEventPage(page)
-    }
-
-    setDisplayValue = (e) => {
-        const { pagesCount } = this.props;
-        const nextPage = e.currentTarget.value;
-        if (nextPage >= 1 && nextPage <= pagesCount) {
-            this.setState({ inputError: false, displayNumber: nextPage })
-        } else {
-            this.setState({ displayNumber: nextPage })
-            this.setState({ inputError: true })
         }
     }
 
@@ -73,41 +28,34 @@ class Users extends React.Component {
         this.setState( { pagginator: !this.state.pagginator } )
     }
 
+    
     render = () => {
         
         const {users, isLoading, currentPage, pagesCount } = this.props;
         const { toggleFollow } =this.props;
-        const { inputError, pagginator } = this.state;
+        const { pagginator } = this.state;
+        const disabled = currentPage === pagesCount ? true : false
         
         const usersList = users.map((user) => <User user={user} toggleFollow={toggleFollow} key={user.id} />);
-
         return (
             <div className={styles.usersPage}>
                 {isLoading && <img src={preloader} alt='preloader' className={styles.preloader} />}
                 {pagginator
-                    ? <section className={styles.pagginatorWrapper}>
-                        <nav className={styles.paginator}>
-                            {(currentPage !== 1) && <span onClick={() => this.setFirstLastPage(1)}>First</span>}
-                            <input type='number' value={this.state.displayNumber}
-                                min='1' max={pagesCount}
-                                className={inputError ? styles.inpError : ''}
-                                onClick={this.setPageOnEvent}
-                                onKeyPress={this.setPageOnKey}
-                                onChange={this.setDisplayValue} />
-                            {(currentPage !== pagesCount)
-                                && <span onClick={() => this.setFirstLastPage(pagesCount)}>Last</span>}
-                        </nav>
-                        <div className={styles.hidePagginator} onClick={this.togglePagginator}>Hide</div>
-                    </section>
+                    ? <div>
+                        <button onClick={this.showMoreUsers} disabled={disabled}>Show more</button>
+                        <Pagginator
+                            startPage={1} finishPage={pagesCount}
+                            currentPage={currentPage} dispatchNewPage={this.setEventPage} />
+                        <p className={styles.pagginatorLink} onClick={this.togglePagginator}>Hide</p>
+                        
+                    </div>
                     : <div>
-                        <button onClick={this.showMoreUsers} disabled={this.state.disabled}>Show more</button>
-                        <p className={styles.pagginatorLable} onClick={this.togglePagginator}>Pagginator</p>
+                        <button onClick={this.showMoreUsers} disabled={disabled}>Show more</button>
+                        <p className={styles.pagginatorLink} onClick={this.togglePagginator}>Pagginator</p>
                     </div>
                 }
 
                 {usersList}
-                {this.state.pagginator
-                    && <button onClick={this.showMoreUsers} disabled={this.state.disabled}>Show more</button>}
             </div>
         )
     }
@@ -115,29 +63,3 @@ class Users extends React.Component {
 
 
 export default Users;
-
-
-
-
-// ------------- получение активной и двух соседний номеров страниц-----------
-/*  const getUsersNumbers = () => {
-            let numbers = [1, 2, 3];
-            let newNumbers = [];
-            if (currentPage > 2 && currentPage < pagesCount) {
-                for (let i = currentPage - 1; i <= currentPage + 1; i++) {
-                    newNumbers = [...newNumbers, i]
-                }
-                numbers = newNumbers;
-            }
-            if (currentPage === pagesCount) {
-                numbers = [pagesCount - 2, pagesCount - 1, pagesCount]
-            }
-            return numbers
-        }
-
-        const pageNumbers = [
-            getUsersNumbers().map((num) => 
-                <span className={ num === currentPage ? styles.active: '' } 
-                    onClick={() => this.setCurrentPage(num)}>{num}</span>
-            )
-        ] */
