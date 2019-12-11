dccfa3f0d74bb18a891fff0b310d5c4e8df1187f
SerejKa-lab <seruogacom@mail.ru>
2019-12-11 12:09:48 +0300

    link for logo image

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className={style.header}>
-            <Link to='/' exact>
+            <Link to='/profile' exact>
                 <img alt='Здесь должен быть логотип' src='https://img2.freepng.ru/20180419/kle/kisspng-logo-canon-clip-art-icon-round-logo-design-5ad87fcf006098.6307124815241379350016.jpg'></img>
             </Link>
         </header>
