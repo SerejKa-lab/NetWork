2f13b0788d91fc1187479aa8b2c9105c245a11f6
SerejKa-lab <seruogacom@mail.ru>
2019-12-24 13:15:28 +0300

    Login component is created; Field, reduxForm, handleSubmit from 'redux-form'

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import styles from './Login.module.css'
 
 
-const Login = () => {
+let Login = (props) => {
+
+    const { handleSubmit, reset } = props
+
+    const submit = (values) => {
+        console.log(values)
+    }
+
     return (
-        <h2>Login, please</h2>
+        <form className={styles.loginForm} onSubmit={handleSubmit(submit)}>
+            <fieldset>
+                <legend>Authorization form</legend>
+                <p><Field name='email' component='input' type='email' placeholder='email' /></p>
+                <p><Field name='password' component='input' type='password' placeholder='Password' /></p>
+                <p><Field name='rememberMe' component='input' type='checkbox'/>Remember me</p>
+            </fieldset>
+            <button type='button' onClick={reset} className={styles.button}>Reset</button>
+            <button type='submit' value='Submit' className={styles.button}>Submit</button>
+        </form>
     )
 }
 
+Login = reduxForm({
+    form: 'login' // имя формы в state (state.form.post)
+})(Login)
+                
 export default Login;
