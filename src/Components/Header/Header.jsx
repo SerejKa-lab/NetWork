31d5077ca6219a1c328f5afd02825d1c6ff10a98
SerejKa-lab <seruogacom@mail.ru>
2020-02-25 21:59:39 +0300

    Header -> logout button on click outside handler

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,47 +1,57 @@
-import React, { useState } from 'react';
-import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
-import avatar from '../../Assets/Images/avatar.png';
-import unlogged from '../../Assets/Images/unlogged.png';
+import React from 'react'
+import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
+import avatar from '../../Assets/Images/avatar.png'
+import unlogged from '../../Assets/Images/unlogged.png'
 import logo from '../../Assets/Images/network_logo.jpg'
+import onClickOutside from 'react-onclickoutside'
 
 
-const Header = (props) => {
+class Header extends React.Component {
 
-    const { login, isAuth, logOut } = props;
-    const [isDisplayed, showLogOut] = useState(false);
-    const logOutStyle = !isDisplayed ? `${styles.logOut} ${styles.logOut_hide}` : styles.logOut;
-    const loggedInStyle = !isDisplayed ? styles.loggedIn : styles.loggedIn+' '+styles.loggedIn_click;
-
-    const toggleLogOut = () => showLogOut(!isDisplayed)
-    
-    const logOutOnClick = () => {
-        logOut()
-        toggleLogOut()
+    state = {
+        isDisplayed: false
     }
 
-    return (
-        <header className={styles.header}>
-            <div className={styles.header_wrapper}>
-                <Link to='/profile'>
-                    <img alt='Logo' src={logo}></img>
-                </Link>
-                {isAuth
-                    ? <div className={styles.loggedIn_wraper}>
-                        <div className={loggedInStyle} onClick={toggleLogOut} >
-                            <span>{login}</span>
-                            <img src={avatar} alt='profile' />
+    handleClickOutside = () => this.setState({isDisplayed: false})
+
+    render() {
+        
+        const { isDisplayed } = this.state
+        const { login, isAuth, logOut } = this.props
+        const logOutStyle = !isDisplayed ? `${styles.logOut} ${styles.logOut_hide}` : styles.logOut
+        const loggedInStyle = !isDisplayed ? styles.loggedIn : styles.loggedIn + ' ' + styles.loggedIn_click
+
+        const toggleLogOut = () => this.setState({isDisplayed: !isDisplayed})
+
+        const logOutOnClick = () => {
+            logOut()
+            toggleLogOut()
+        }
+
+        return (
+            <header className={styles.header}>
+                <div className={styles.header_wrapper}>
+                    <Link to='/profile'>
+                        <img alt='Logo' src={logo}></img>
+                    </Link>
+                    {isAuth
+                        ? <div className={styles.loggedIn_wraper}>
+                            <div className={loggedInStyle} onClick={toggleLogOut} >
+                                <span>{login}</span>
+                                <img src={avatar} alt='profile' />
+                            </div>
+                            <span onClick={logOutOnClick} className={logOutStyle}>LogOut</span>
                         </div>
-                        <span onClick={ logOutOnClick } className={logOutStyle}>LogOut</span>
-                    </div>
-                    : <div className={styles.login}>
-                        <Link to='/login'>Login</Link>
-                        <img src={unlogged} alt='profile' />
-                    </div>
-                }
-            </div>
-        </header>
-    )
+                        : <div className={styles.login}>
+                            <Link to='/login'>Login</Link>
+                            <img src={unlogged} alt='profile' />
+                        </div>
+                    }
+                </div>
+            </header>
+        )
+    }
 }
 
-export default Header;
+export default onClickOutside(Header)
